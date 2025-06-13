c4f23fa3db40ef329e215a375049f5a6dffb506f
jzakotnik <jure@zakotnik.de>
2025-06-13 16:51:39 +0200

    updated with removal of bootstrap

--- start.sh
+++ start.sh
@@ -69,3 +69,6 @@ if [ "$FRONTEND_DEV" = "true" ]; then
     npm i
     npm run dev
 fi
+
+# to be on the safe side, remove all ipfs bootstrap peers
+docker compose exec ipfs0 ipfs bootstrap rm --all
