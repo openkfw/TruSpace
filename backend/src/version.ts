be11925e6d63364660de8d0797c0c21dd3d0d04a
Samuel Pull <samuel.pull@accenture.com>
2025-08-11 11:12:01 +0200

    Deleted old files

--- backend/src/version.ts
+++
@@ -1,4 +0,0 @@
-// Auto-generated - DO NOT EDIT
-export const COMMIT_HASH = 'e48d1a4014e1040b8081458eaf76cb9864a0bad1';
-export const SHORT_COMMIT_HASH = 'e48d1a40';
-export const COMMIT_DATE = '2025-08-11 10:28:28 +0200';
