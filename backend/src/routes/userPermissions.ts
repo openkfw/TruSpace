738198bc9728e4c6dded72126585bed37c047f62
maxjmohr <mamohoc@yahoo.com>
2026-04-02 15:12:59 +0200

    When deleting owner of workspace, assign new owner

--- backend/src/routes/userPermissions.ts
+++ backend/src/routes/userPermissions.ts
@@ -11,6 +11,7 @@ import {
   findPermissionById,
   findUsersInWorkspace,
   removePermission,
+  selectNextOldestPermissionUser,
   UserPermissionDto,
 } from "../handlers/userPermissions";
 
@@ -131,6 +132,7 @@ router.delete(
       // We delete all found permissions
       // Additionally: for each permission found, check the workspace if there are other users and if it is private.
       // If there are no other users, delete the workspace and all its data (files, folders, etc.)
+      // If there are other users and the deleted user is an owner, assign the owner role to the next oldest user
 
       for (const permission of permissions) {
         if (!permission.id || !permission.workspaceId) {
@@ -141,9 +143,25 @@ router.delete(
         const client = new IpfsClient();
         const workspace = await client.getWorkspaceById(permission.workspaceId);
         const users = await findUsersInWorkspace(permission.workspaceId);
-        if (users.length === 0 && workspace.length > 0 && !workspace[0].meta.is_public) {
-          const wCID = workspace[0].cid;
-          await client.deleteWorkspaceById(wCID, permission.workspaceId);
+
+        if (workspace.length > 0 && !workspace[0].meta.is_public) {
+          if (users.length === 0) {
+            const wCID = workspace[0].cid;
+            await client.deleteWorkspaceById(wCID, permission.workspaceId);
+
+          } else if (permission.role === "owner") {
+            const nextOwner = selectNextOldestPermissionUser(users);
+
+            if (nextOwner?.id && nextOwner.email) {
+              await removePermission(nextOwner.id);
+              await createPermission({
+                workspaceId: permission.workspaceId,
+                email: nextOwner.email,
+                role: "owner",
+                created_at: nextOwner.created_at,
+              });
+            }
+          }
         }
 
       }
